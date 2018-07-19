52309bb2830f6a16c6de54eed8b7f5704339cd93
Charles Neau <charles63500@gmail.com>
2018-07-19 14:20:57 +0100

    init

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/chneau/anecdote/pkg/anecdote"
+)
+
+var (
+	source string
+)
+
+func init() {
+	gracefulExit()
+	flag.StringVar(&source, "source", "SI", "source")
+	flag.Parse()
+}
+
+func gracefulExit() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	go func() {
+		<-quit
+		os.Exit(0)
+	}()
+}
+
+// checkError
+func ce(err error, msg string) {
+	if err != nil {
+		log.Panicln(msg, err)
+	}
+}
+
+func main() {
+	aa := []anecdote.Anecdote{}
+	var err error
+	switch source {
+	case "SCMB":
+		aa, err = anecdote.SCMB()
+		ce(err, "anecdote")
+	case "SI":
+		aa, err = anecdote.SI()
+		ce(err, "anecdote")
+	default:
+		aa, err = anecdote.SCMB()
+		ce(err, "anecdote")
+	}
+	fmt.Println(aa[0].String())
+}
---
+++ pkg/anecdote/anecdote.go
@@ -0,0 +1,70 @@
+package anecdote
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// Anecdote ...
+type Anecdote struct {
+	Title   string
+	Summary string
+}
+
+// String ...
+func (a *Anecdote) String() string {
+	result := ""
+	if a.Title != "" {
+		result += a.Title + "\n"
+	}
+	result += a.Summary
+	return result
+}
+
+// SCMB Se Coucher Moins Bete
+func SCMB() ([]Anecdote, error) {
+	found := []Anecdote{}
+	res, err := http.Get("https://secouchermoinsbete.fr/random")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find(".anecdote").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("header h1 a").Text()
+		summary := s.Find(".summary a").Text()
+		summary = strings.Replace(summary, "\n                En savoir plus", "", -1)
+		found = append(found, Anecdote{
+			Summary: summary,
+			Title:   title,
+		})
+	})
+	return found, nil
+}
+
+// SI Savoir Inutile
+func SI() ([]Anecdote, error) {
+	found := []Anecdote{}
+	res, err := http.Get("https://www.savoir-inutile.com/")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	doc.Find("#phrase").Each(func(i int, s *goquery.Selection) {
+		summary := s.Text()
+		found = append(found, Anecdote{
+			Summary: summary,
+			Title:   "",
+		})
+	})
+	return found, nil
+}
