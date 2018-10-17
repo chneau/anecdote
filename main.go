492f063cc86451aa1c4fe64de385639a18006531
chneau <charles63500@gmail.com>
2018-10-17 20:20:15 +0100

    added nb ad 2nd paramter

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"os"
+	"strconv"
+
+	"github.com/chneau/limiter"
 
 	"github.com/chneau/anecdote/pkg/anecdote"
 )
+
+const line = "############################################################################"
 
 func printUsage() {
 	println(`Possible values:`)
@@ -15,16 +20,34 @@ func printUsage() {
 
 func main() {
 	source := "SCMB"
+	nb := 1
 	if len(os.Args) > 1 {
 		source = os.Args[1]
 	}
-	if v, exist := anecdote.Sources[source]; exist {
-		aa, err := v.Anecdotes()
+	if len(os.Args) > 2 {
+		var err error
+		nb, err = strconv.Atoi(os.Args[2])
 		if err != nil {
 			panic(err)
 		}
-		println(aa[0].String())
+	}
+	if _, exist := anecdote.Sources[source]; !exist {
+		printUsage()
 		return
 	}
-	printUsage()
+	limit := limiter.New(nb)
+	for i := 0; i < nb; i++ {
+		limit.Execute(func() {
+			if v, exist := anecdote.Sources[source]; exist {
+				aa, err := v.Anecdotes()
+				if err != nil {
+					panic(err)
+				}
+				println(line + "\n" + aa[0].String())
+				return
+			}
+		})
+	}
+	limit.Wait()
+	println(line)
 }
