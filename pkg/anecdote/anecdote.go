e2232228ea17437202fb186b862e7f712330c4e6
Charles Neau <charles63500@gmail.com>
2018-10-17 15:31:50 +0100

    refactor + added  RW

--- pkg/anecdote/anecdote.go
+++ pkg/anecdote/anecdote.go
@@ -12,92 +12,106 @@ import (
 type Anecdote struct {
 	Title   string
 	Summary string
+	Example string
 }
 
 // String ...
 func (a *Anecdote) String() string {
 	result := ""
 	if a.Title != "" {
-		result += a.Title + "\n"
+		result += a.Title
 	}
-	result += a.Summary
+	if a.Summary != "" {
+		if result != "" {
+			result += "\n"
+		}
+		result += a.Summary
+	}
+	if a.Example != "" {
+		if result != "" {
+			result += "\n"
+		}
+		result += a.Example
+	}
 	return result
 }
 
 // Sources ...
 var Sources map[string]func() ([]Anecdote, error)
 
+// Descriptor of source ...
+type Descriptor struct {
+	URL     string
+	Content string
+	Title   string
+	Summary string
+	Example string
+	Replace string
+}
+
 func init() {
 	Sources = map[string]func() ([]Anecdote, error){}
-	Sources["SCMB"] = scmb
-	Sources["SI"] = si
-	Sources["D2R"] = d2r
+	Sources["SCMB"] = Builder(Descriptor{
+		URL:     "https://secouchermoinsbete.fr/random",
+		Content: ".anecdote",
+		Title:   "header h1 a",
+		Summary: ".summary a",
+		Replace: "\n                En savoir plus",
+	})
+	Sources["SI"] = Builder(Descriptor{
+		URL:     "https://www.savoir-inutile.com/",
+		Summary: "#phrase",
+	})
+	Sources["D2R"] = Builder(Descriptor{
+		URL:     "http://www.dico2rue.com/mots-au-hasard/",
+		Content: ".word_center",
+		Title:   "td.word",
+		Summary: "tbody > tr:nth-child(2)",
+		Example: "td.example",
+	})
+	Sources["RW"] = Builder(Descriptor{
+		URL:     "https://randomword.com/",
+		Title:   "#random_word",
+		Summary: "#random_word_definition",
+	})
 }
 
-func scmb() ([]Anecdote, error) {
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-	found := []Anecdote{}
-	res, err := client.Get("https://secouchermoinsbete.fr/random")
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	doc.Find(".anecdote").Each(func(i int, s *goquery.Selection) {
-		title := s.Find("header h1 a").Text()
-		summary := s.Find(".summary a").Text()
-		summary = strings.Replace(summary, "\n                En savoir plus", "", -1)
-		found = append(found, Anecdote{
-			Summary: summary,
-			Title:   title,
+// Builder returns annecdotes funcs
+func Builder(d Descriptor) func() ([]Anecdote, error) {
+	return func() ([]Anecdote, error) {
+		client := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+		found := []Anecdote{}
+		res, err := client.Get(d.URL)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			return nil, err
+		}
+		if d.Content == "" {
+			d.Content = "body"
+		}
+		doc.Find(d.Content).Each(func(i int, s *goquery.Selection) {
+			a := Anecdote{}
+			if d.Title != "" {
+				a.Title = strings.TrimSpace(s.Find(d.Title).Text())
+			}
+			if d.Summary != "" {
+				a.Summary = strings.TrimSpace(s.Find(d.Summary).Text())
+				a.Summary = strings.Replace(a.Summary, d.Replace, "", -1)
+			}
+			if d.Example != "" {
+				a.Example = strings.TrimSpace(s.Find(d.Example).Text())
+				a.Example = strings.Replace(a.Example, d.Replace, "", -1)
+			}
+			found = append(found, a)
 		})
-	})
-	return found, nil
-}
-
-func si() ([]Anecdote, error) {
-	found := []Anecdote{}
-	res, err := http.Get("https://www.savoir-inutile.com/")
-	if err != nil {
-		return nil, err
+		return found, nil
 	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	found = append(found, Anecdote{
-		Summary: doc.Find("#phrase").First().Text(),
-		Title:   "",
-	})
-	return found, nil
-}
-
-func d2r() ([]Anecdote, error) {
-	found := []Anecdote{}
-	res, err := http.Get("http://www.dico2rue.com/mots-au-hasard/")
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	body := doc.Find(".word_center").First()
-	word := strings.TrimSpace(body.Find("td.word").First().Text())
-	def := strings.TrimSpace(body.Find("tbody > tr:nth-child(2)").First().Text())
-	ex := strings.TrimSpace(body.Find("td.example").First().Text())
-	found = append(found, Anecdote{
-		Summary: def + "\n" + ex,
-		Title:   word,
-	})
-	return found, nil
 }
