549354d01b71dcd8b2fe1b60cb1e2c7dee5b14e9
Charles Neau <charles63500@gmail.com>
2018-10-17 16:21:18 +0100

    refactor

--- main.go
+++ main.go
@@ -2,30 +2,9 @@ package main
 
 import (
 	"os"
-	"os/signal"
 
 	"github.com/chneau/anecdote/pkg/anecdote"
 )
-
-func init() {
-	gracefulExit()
-}
-
-func gracefulExit() {
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	go func() {
-		<-quit
-		os.Exit(0)
-	}()
-}
-
-// checkError
-func ce(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
 func printUsage() {
 	println(`Possible values:`)
@@ -41,7 +20,9 @@ func main() {
 	}
 	if v, exist := anecdote.Sources[source]; exist {
 		aa, err := v.Anecdotes()
-		ce(err)
+		if err != nil {
+			panic(err)
+		}
 		println(aa[0].String())
 		return
 	}
